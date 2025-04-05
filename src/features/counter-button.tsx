66c1206928ab8e0ed96f0cc1d248164ea10ba935
Gabor Soos <gabor.soos@byborgenterprises.com>
2025-04-05 21:03:54 +0200

    add move postcss config to package.json to be able to import css in tests

--- src/features/counter-button.tsx
+++ src/features/counter-button.tsx
@@ -2,12 +2,13 @@
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import "@/features/counter-button.style.css";
 
 export const CounterButton = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <Button className="cursor-pointer" onClick={() => setCount(count + 1)}>
+    <Button className="cursor-pointer red-text" onClick={() => setCount(count + 1)}>
       Counter: {count}
     </Button>
   );
